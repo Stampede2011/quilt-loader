0f24ad16279055f8e8f1d335886d5bee2c14c176
Stampede <carterblowers01@gmail.com>
2023-05-31 13:37:46 -0500

    GOODBYE SPAM!

--- src/main/java/org/quiltmc/loader/impl/filesystem/QuiltClassPath.java
+++ src/main/java/org/quiltmc/loader/impl/filesystem/QuiltClassPath.java
@@ -315,7 +315,7 @@ public class QuiltClassPath {
 						sb.append(path);
 					}
 				}
-				Log.warn(LogCategory.GENERAL, sb.toString());
+				//Log.warn(LogCategory.GENERAL, sb.toString()); DUMBEST SHIT I EVER DAMN SEEN
 			}
 			return paths.get(0);
 		}
